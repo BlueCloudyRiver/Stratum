f3d4946465c1ce29beb71b13fdbbe774e6315cb2
BlueCloudyRiver <bluecloudyriver@gmail.com>
2026-06-06 08:35:13 +0530

    update postgres database connection, implement jwt authentication, and sql migrations framework

---
+++ cmd/api/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"StratumAPI/internal/database"
+	"StratumAPI/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env", "../../.env"); err != nil {
+		log.Println("Warning: No .env file found, reading straight from system environment")
+	}
+
+	if err := database.Connect(); err != nil {
+		log.Fatalf("Database initialization failed: %v", err)
+	}
+	fmt.Println("Yay! Connected to PostgreSQL securely")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Post("/register", handlers.RegisterHandler)
+	r.Post("/login", handlers.LoginHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server starting on port :%s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module stratum
+module StratumAPI
 
 go 1.26.3
 
 require (
-	github.com/go-chi/chi/v5 v5.3.0 
-	github.com/golang-jwt/jwt/v5 v5.3.1 
+	github.com/go-chi/chi/v5 v5.3.0
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.12.3
-	golang.org/x/crypto v0.52.0 
+	golang.org/x/crypto v0.52.0
 )
