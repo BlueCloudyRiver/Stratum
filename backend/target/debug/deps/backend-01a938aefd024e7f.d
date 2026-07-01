e457eaed4b225136ea9b499d27dd49c8df751a3c
BlueCloudyRiver <bluecloudyriver@gmail.com>
2026-07-01 21:08:08 +0530

    redesign landing page and add dependencies for rust backend

--- backend/target/debug/deps/backend-01a938aefd024e7f.d
+++
@@ -1,5 +0,0 @@
-E:\Moon\Lab\Stratum\backend\target\debug\deps\backend-01a938aefd024e7f.d: src\main.rs
-
-E:\Moon\Lab\Stratum\backend\target\debug\deps\libbackend-01a938aefd024e7f.rmeta: src\main.rs
-
-src\main.rs:
